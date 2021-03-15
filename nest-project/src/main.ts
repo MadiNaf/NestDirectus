14eed5cd8016ee085a62e5093c447505b02d55f5
madi_naf <bornetocode42@gmail.com>
2021-03-15 10:25:42 +0100

    add nest expresse

--- nest-project/src/main.ts
+++ nest-project/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const DirectusSDK = require('@directus/sdk-js');
+import { NestExpressApplication } from '@nestjs/platform-express';
 export const directus = new DirectusSDK('http://localhost:8055/');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
