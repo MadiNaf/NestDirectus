9e28dd250703a2cdb257eb864a3305f3da265e54
madi_naf <bornetocode42@gmail.com>
2021-03-15 10:53:33 +0100

    use directus uploads directory for a static assets storage

--- nest-project/src/main.ts
+++ nest-project/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const DirectusSDK = require('@directus/sdk-js');
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+const DirectusSDK = require('@directus/sdk-js');
 export const directus = new DirectusSDK('http://localhost:8055/');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  // use the directus uploads directory for a static assets storage
+  app.useStaticAssets(join(__dirname, '../..', '/directus-project/uploads/'))
   await app.listen(3000);
 }
 bootstrap();
